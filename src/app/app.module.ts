81af897efc707cb19629587003f9bfdfef6e9300
SUB_ACCOUNT <SUB_ADDRESS>
2019-11-21 11:50:05 +0800

    added component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { WeatherCardComponent } from './ui/weather-card/weather-card.component';
+import { AddCardComponent } from './ui/add-card/add-card.component';
+import { DetailsComponent } from './pages/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WeatherCardComponent,
+    AddCardComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
